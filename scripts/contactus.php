842d31e2010e091b14cc0f2a53a1d32043394267
codveda <srinivas.chikkala@gmail.com>
2018-08-21 09:59:05 +0530

    Initial commit

---
+++ scripts/contactus.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "srinivas.chikkala@innovista.co.in";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
